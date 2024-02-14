141093769415fd4083e4c90ec3d579cb6a3f49b4
LilDie <neobad.reyes@gmail.com>
2024-02-14 14:49:43 -0300

    a

--- aca-estamos-project/src/componentes/OlvidoContraseña/OlvidoContraseña.jsx
+++ aca-estamos-project/src/componentes/OlvidoContraseña/OlvidoContraseña.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import './Olvido-Contra-envio-Correo.css'
+import './OlvidoContraseña.css';
+
 const OlvidoContraseña = () => {
- 
-  
   return (
     <main className="d-flex justify-content-center vh-100 w-100">
       <div className="row">
         <div id="ContenedorOlvCont" className="col-lg-8 offset-lg-2 col-md-10 offset-md-1 align-self-center">
-          <div className="border p-4 mb-4 mb-lg-5 mx-lg-1"
-            style={{
-              borderRadius: '20px',
-              borderStyle: 'solid',
-              borderColor: 'black',
-              boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
-              wordWrap: 'break-word'
-            }}
-          >
+          <div className="border p-4 mb-4 mb-lg-5 mx-lg-1">
             <h2 className="text-center">Recuperación de contraseña</h2>
             <div className="from-ingreso">
               <form>
@@ -25,7 +16,7 @@ const OlvidoContraseña = () => {
                   <div id="emailHelp" className="form-text">Ingrese un correo electrónico registrado</div>
                 </div>
                 <div className="d-flex justify-content-between">
-                  <button type="button" className="btn btn-secondary btn-lg" id="btn-back" style={{ marginRight: '5px' }}>Volver</button>
+                  <button type="button" className="btn btn-secondary btn-lg" id="btn-back">Volver</button>
                   <button type="submit" className="btn btn-primary btn-lg" id="bt-login">Recuperar Contraseña</button>
                 </div>
               </form>
