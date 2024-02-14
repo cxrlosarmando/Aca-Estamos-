f8ca99e33e804f6fcad29436343b8067cacd79c8
Daniela Zuñiga <nta.danaz@gmail.com>
2024-02-14 18:07:03 -0300

    Panel admin DZ

--- aca-estamos-project/src/componentes/PagesAdministrador/PanelAdmin/PanelAdmin.jsx
+++ aca-estamos-project/src/componentes/PagesAdministrador/PanelAdmin/PanelAdmin.jsx
@@ -1,57 +1,52 @@
+import React from 'react';
 import './PanelAdmin.css';
 
 const PanelAdmin = () => {
   return (
     <>
-      <main className="solicitudescv">
-        <div className="container-fluid" style={{ borderRadius: '10px', backgroundColor: 'rgba(4, 157, 217, 0.15)', boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', width: '100%' }}>
-          <div className="solici-row">
-            <div className="solici-col1">
-              <img src="../Img/user-perfil.png" alt="Imagen perfil" id="user-perfil" />
+      <main className="perfiladmin">
+        <div className="container-fluid" style={{ borderRadius: '10px', borderColor: 'black', boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', width: '100%', textAlign: 'center' }}>
+          <div className="admin-row">
+            <div className="admin-col1">
+              <button id="btn-empresas" type="button" className="btn btn-primary btn-lg">Empresas</button>
             </div>
-            <div className="solici-col2">
-              <h3 className="Curriculum">Curriculum</h3>
-              <p><a href="#">Mostrar CV Generado</a></p>
+            <div className="admin-col2">
+              <button id="btn-solicitudes" className="btn btn-primary btn-lg">Solicitudes</button>
             </div>
-            <div className="solici-col3">
-              <button id="btn-Aprobar" className="btn btn-primary btn-lg">Aprobar</button>
-              <button id="btn-Denegar" className="btn btn-secondary btn-lg">Denegar</button>
+            <div className="admin-col3">
+              <button id="btn-ofertas" className="btn btn-secondary btn-lg">Nuevas Ofertas</button>
             </div>
           </div>
         </div>
       </main>
 
-      <main className="solicitudescv">
-        <div className="container-fluid" style={{ borderRadius: '10px', backgroundColor: 'rgba(4, 157, 217, 0.15)', boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', width: '100%' }}>
-          <div className="solici-row">
-            <div className="solici-col1">
-              <img src="../Img/user-perfil.png" alt="Imagen perfil" id="user-perfil" />
+      <main className="perfiladmin">
+        <div className="container-fluid" style={{ borderRadius: '10px', borderColor: 'black', boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', width: '100%', textAlign: 'center' }}>
+          <div className="admin-row">
+            <div className="admin-col1">
+              <button id="btn-usuario" type="button" className="btn btn-primary btn-lg" style={{ backgroundColor: '#049DD9', borderColor: '#049DD9' }}>Usuarios</button>
             </div>
-            <div className="solici-col2">
-              <h3 className="Curriculum">Curriculum</h3>
-              <p><a href="#">Mostrar CV Generado</a></p>
+            <div className="admin-col2">
+              <button id="btn-solicitudes" className="btn btn-primary btn-lg">Solicitudes</button>
             </div>
-            <div className="solici-col3">
-              <button id="btn-Aprobar" className="btn btn-primary btn-lg">Aprobar</button>
-              <button id="btn-Denegar" className="btn btn-secondary btn-lg">Denegar</button>
+            <div className="admin-col3">
+              <button id="btn-newcv" className="btn btn-secondary btn-lg">Nuevos CV</button>
             </div>
           </div>
         </div>
       </main>
 
-      <main className="solicitudescv">
-        <div className="container-fluid" style={{ borderRadius: '10px', backgroundColor: 'rgba(4, 157, 217, 0.15)', boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', width: '100%' }}>
-          <div className="solici-row">
-            <div className="solici-col1">
-              <img src="../Img/user-perfil.png" alt="Imagen perfil" id="user-perfil" />
+      <main className="perfiladmin">
+        <div className="container-fluid" style={{ borderRadius: '10px', borderColor: 'black', boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', width: '100%', textAlign: 'center' }}>
+          <div className="admin-row">
+            <div className="admin-col1">
+              <button id="btn-perfil" type="button" className="btn btn-primary btn-lg">Perfiles</button>
             </div>
-            <div className="solici-col2">
-              <h3 className="Curriculum">Curriculum</h3>
-              <p><a href="#">Mostrar CV Generado</a></p>
+            <div className="admin-col2">
+              <button id="btn-activos" className="btn btn-primary btn-lg">Activos</button>
             </div>
-            <div className="solici-col3">
-              <button id="btn-Aprobar" className="btn btn-primary btn-lg">Aprobar</button>
-              <button id="btn-Denegar" className="btn btn-secondary btn-lg">Denegar</button>
+            <div className="admin-col3">
+              <button id="btn-inactivos" className="btn btn-secondary btn-lg">Inactivos</button>
             </div>
           </div>
         </div>
