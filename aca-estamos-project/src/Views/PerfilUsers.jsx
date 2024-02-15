8537d605a5abea5eebbe42ad0188703fd7aa1071
Daniela Zuñiga <nta.danaz@gmail.com>
2024-02-15 14:19:13 -0300

    perfil ruta DZ

---
+++ aca-estamos-project/src/Views/PerfilUsers.jsx
@@ -0,0 +1,17 @@
+import Footer from "../componentes/Footer/Footer";
+import Header from "../componentes/Header/Header";
+import Navbar from "../componentes/Navbar/Navbar";
+import PerfilUsuario from "../componentes/PagesUsuario/PerfilUsuario/PerfilUsuario"
+
+const PerfilUsers = () => {
+  return (
+    <>
+    <Header />
+    <Navbar />
+    <PerfilUsuario />
+    <Footer />
+    </>
+  )
+}
+
+export default PerfilUsers;
