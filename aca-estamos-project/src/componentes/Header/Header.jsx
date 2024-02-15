609ca0233c36a632e37a1da44439a85182c1e2ce
Daniela Zuñiga <nta.danaz@gmail.com>
2024-02-14 23:56:58 -0300

    login DZ

--- aca-estamos-project/src/componentes/Header/Header.jsx
+++ aca-estamos-project/src/componentes/Header/Header.jsx
@@ -18,10 +18,10 @@ const Header = () => {
                 <p className="mb-0">#RencaParticipa</p>
               </div>
               <div className="container-fluid gap-5 d-md-flex justify-content-md-end p-4" id="botonesnav">
-                <button className="btn btn-custom btn-lg me-2" id="botonInicio" type="button" style={{ backgroundColor: '#049DD9' }}>
+                <button className="btn btn-custom btn-lg me-2" href="/Login" id="botonInicio" type="button" style={{ backgroundColor: '#049DD9' }}>
                   Iniciar Sesión
                 </button>
-                <button className="btn btn-custom btn-lg me-2" type="button" id="botonInicio" style={{ backgroundColor: '#D97D0D' }}>
+                <button className="btn btn-custom btn-lg me-2" href="/Login" type="button" id="botonInicio" style={{ backgroundColor: '#D97D0D' }}>
                   Regístrate
                 </button>
               </div>
--- aca-estamos-project/src/componentes/InicioSesion/InicioSesion.jsx
+++ aca-estamos-project/src/componentes/InicioSesion/InicioSesion.jsx
@@ -42,4 +42,4 @@ export const InicioSesion = () => {
         </>
     );
 };
-export default Inicio-Sesion;
+export default InicioSesion;
