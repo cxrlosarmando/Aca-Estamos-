2fc62bd0c29efaef43e5e15931c5a75b1765493b
Daniela Zuñiga <nta.danaz@gmail.com>
2024-02-15 20:01:01 -0300

    login form

--- aca-estamos-project/src/Router/routes.jsx
+++ aca-estamos-project/src/Router/routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import ErrorComponent from '../Views/Cursos';
 import Home from "../Views/Home";
 import Login from "../Views/Login";
-/* import LoginFormulario from '../Views/LoginFormulario'; */
+import LoginFormulario from '../Views/LoginFormulario';
 import PerfilUsers from '../Views/PerfilUsers';
 
 const router = createBrowserRouter([
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
   { path: '/Nosotros', element: <ErrorComponent statusCode={404} /> },
   { path: '/Contacto', element: <ErrorComponent statusCode={404} /> },
   { path: '/Login', element: <Login /> },
-  /* { path: '/Login-form', element: <LoginFormulario /> }, */
+  { path: '/Login-form', element: <LoginFormulario /> },
   { path: '/Perfil-Usuario', element: <PerfilUsers /> },
 ]);
 
