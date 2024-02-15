ed179852334fcfa393bfdec3587ec6abb2936c99
richard mejias <richard987@hotmail.cl>
2024-02-15 12:50:50 -0300

    alertas-rm

--- aca-estamos-project/src/App.jsx
+++ aca-estamos-project/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 import './App.css';
 import Loading from './Efectos/Loading';
+import Alert from './Efectos/Alert';
+import React from 'react';
 
 function App() {
 
   return (
     <>
+      <div>
+        <h1>Alertas con useAlert</h1>
+        <Alert /> {/* Renderiza el componente de alerta */}
+      </div>
     </>
   );
 }
- 
+
 export default App;
