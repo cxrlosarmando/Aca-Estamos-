9a76219e1164067744ab2903ee61565814c5343b
Daniela Zuñiga <nta.danaz@gmail.com>
2024-02-16 10:55:33 -0300

    loading Pefil 1000 DZ

--- aca-estamos-project/src/Views/PerfilAdmin.jsx
+++ aca-estamos-project/src/Views/PerfilAdmin.jsx
@@ -10,7 +10,7 @@ const PerfilAdmin = () => {
   useEffect(() => {
       setTimeout(() => {
           setLoading(false);
-      }, 2000);
+      }, 1000);
   }, []);
 
   return (
--- aca-estamos-project/src/Views/PerfilEmpresas.jsx
+++ aca-estamos-project/src/Views/PerfilEmpresas.jsx
@@ -10,7 +10,7 @@ const PerfilEmpresas = () => {
   useEffect(() => {
       setTimeout(() => {
           setLoading(false);
-      }, 2000);
+      }, 1000);
   }, []);
 
   return (
