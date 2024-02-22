3c7dc932439e3d4f663e822532e971bebe7749a0
Daniela Zuñiga <nta.danaz@gmail.com>
2024-02-21 21:57:02 -0300

    estructura back dz

---
+++ aca-estamos-back/src/router/router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+//Router   USUARIOS //
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+//Router   EMPRESAS //
+
+
+//Router   ADMINISTRADOR //
+
+
+
+
+module.exports = router;
