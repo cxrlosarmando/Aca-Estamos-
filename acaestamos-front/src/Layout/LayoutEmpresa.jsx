ad2d6cd97981f875477fb7e2a840d92eb0c5a874
LilDie <neobad.reyes@gmail.com>
2024-03-01 01:13:35 -0300

    retoques finales DC

--- acaestamos-front/src/Layout/LayoutEmpresa.jsx
+++ acaestamos-front/src/Layout/LayoutEmpresa.jsx
@@ -1,7 +1,7 @@
-import Header from "../componentes/Header/Header";
+
 import Footer from "../componentes/Footer/Footer";
 import NavbarEmpresa from "../componentes/NavbarEmpresa/NavbarEmpresa";
-import Navbar from "../componentes/Navbar/Navbar";
+
 const LayoutEmpresa = ({ children }) => {
   return (
     
