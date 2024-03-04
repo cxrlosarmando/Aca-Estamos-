6d772370591cde5d04e99b6510132d6f29a05020
LilDie <neobad.reyes@gmail.com>
2024-03-04 16:48:47 -0300

    cambios en logica DC

--- acaestamos-front/src/Layout/LayoutEmpresa.jsx
+++ acaestamos-front/src/Layout/LayoutEmpresa.jsx
@@ -1,12 +1,27 @@
 
 import Footer from "../componentes/Footer/Footer";
 import NavbarEmpresa from "../componentes/NavbarEmpresa/NavbarEmpresa";
-
+import { useState, useEffect } from "react";
+import Navbar from "../componentes/Navbar/Navbar";
 const LayoutEmpresa = ({ children }) => {
+
+const [estaLogeado, setEstaLogeado] = useState(false);
+
+useEffect(() => {
+  if (localStorage.getItem("accessToken")) {
+    setEstaLogeado(true);
+  } else {
+    setEstaLogeado(false);
+  }
+}, []);
+
+
+
   return (
     
      <>
-      <NavbarEmpresa />
+     
+     {estaLogeado ? <NavbarEmpresa/> : <Navbar />}   
       <div style={{ flex: 1 }}>{children}</div>
       <Footer />
     </>
