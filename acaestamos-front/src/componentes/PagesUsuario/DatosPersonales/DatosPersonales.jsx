82d630522696f0f385746af5bf439d1e6334935c
richard mejias <richard987@hotmail.cl>
2024-03-04 12:33:33 -0300

    implementacion-alertas-rm

--- acaestamos-front/src/componentes/PagesUsuario/DatosPersonales/DatosPersonales.jsx
+++ acaestamos-front/src/componentes/PagesUsuario/DatosPersonales/DatosPersonales.jsx
@@ -3,9 +3,12 @@ import { Link, useNavigate } from 'react-router-dom';
 import './DatosPersonales.css';
 import client from '../../../Utils/axios.Client';
 import getUserId from '../../../Utils/getUserId';
+import { useAlert } from '../../../Efectos/useAlert'
 
 
 const DatosPersonales = () => {
+
+    //Definicion de constantes
     const [perfil, setPerfil] = useState(null);
     const [Nombre, setNombre] = useState('');
     const [Apellido, setApellido] = useState('');
@@ -20,27 +23,41 @@ const DatosPersonales = () => {
     const [ArchivoPDF4, setArchivoPDF4] = useState(null);
     const [ImagenPerfil, setImagenPerfil] = useState(null);
 
+    //Usa el hook useAlert para obtener el estado y la API de alerta
+    const [alertState, alertApi] = useAlert("alertsElement");
+
+    //Definicion de funciones
+    const showAlert = () => {
+
+        let hasError = true;
+
+        if (!Nombre || !Apellido || !Telefono || !Rubro || !EstadoCivil || !LinkedIn) {
+            alertApi.show("¡No llenaste correctamente los datos solicitados!", 'error');
+            hasError = false;
+        }
+    };
+
     useEffect(() => {
         const obtenerPerfil = async () => {
             try {
 
-                const response = await client.get(`/Obtener-Perfil/${getUserId()}`) 
+                const response = await client.get(`/Obtener-Perfil/${getUserId()}`)
                 setPerfil(response?.data);
 
-                if(response?.data?.Nombre) setNombre(response?.data?.Nombre);
-                if(response?.data?.Apellido) setApellido(response?.data?.Apellido);
-                if(response?.data?.Telefono) setTelefono(response?.data?.Telefono);
-                if(response?.data?.EstadoCivil) setEstadoCivil(response?.data?.EstadoCivil);
-                if(response?.data?.LinkedIn) setLinkedIn(response?.data?.LinkedIn);
-                if(response?.data?.Rubro) setRubro(response?.data?.Rubro);
-                if(response?.data?.Disponibilidad) setDisponibilidad(response?.data?.Disponibilidad);
-                if(response?.data?.ArchivoPDF1) setArchivoPDF1(response?.data?.ArchivoPDF1);
-                if(response?.data?.ArchivoPDF2) setArchivoPDF2(response?.data?.ArchivoPDF2);
-                if(response?.data?.ArchivoPDF3) setArchivoPDF3(response?.data?.ArchivoPDF3);
-                if(response?.data?.ArchivoPDF4) setArchivoPDF4(response?.data?.ArchivoPDF4);
-                if(response?.data?.ImagenPerfil) setImagenPerfil(response?.data?.ImagenPerfil);
+                if (response?.data?.Nombre) setNombre(response?.data?.Nombre);
+                if (response?.data?.Apellido) setApellido(response?.data?.Apellido);
+                if (response?.data?.Telefono) setTelefono(response?.data?.Telefono);
+                if (response?.data?.EstadoCivil) setEstadoCivil(response?.data?.EstadoCivil);
+                if (response?.data?.LinkedIn) setLinkedIn(response?.data?.LinkedIn);
+                if (response?.data?.Rubro) setRubro(response?.data?.Rubro);
+                if (response?.data?.Disponibilidad) setDisponibilidad(response?.data?.Disponibilidad);
+                if (response?.data?.ArchivoPDF1) setArchivoPDF1(response?.data?.ArchivoPDF1);
+                if (response?.data?.ArchivoPDF2) setArchivoPDF2(response?.data?.ArchivoPDF2);
+                if (response?.data?.ArchivoPDF3) setArchivoPDF3(response?.data?.ArchivoPDF3);
+                if (response?.data?.ArchivoPDF4) setArchivoPDF4(response?.data?.ArchivoPDF4);
+                if (response?.data?.ImagenPerfil) setImagenPerfil(response?.data?.ImagenPerfil);
 
-                
+
             } catch (error) {
                 if (error.response) {
                     console.error('Error en la respuesta:', error.response.data);
@@ -55,7 +72,7 @@ const DatosPersonales = () => {
         obtenerPerfil();
     }, []);
 
-    
+
     const navigate = useNavigate();
 
     const handleArchivoPDF1Change = (e) => {
@@ -204,7 +221,7 @@ const DatosPersonales = () => {
                                 <Link to="/Perfil-Usuario" type="button" className="btn btn-secondary btn-lg me-2">
                                     Cancelar
                                 </Link>
-                                <button type="submit" className="btn btn-primary btn-lg">
+                                <button type="submit" className="btn btn-primary btn-lg" onClick={showAlert}>
                                     Guardar
                                 </button>
                             </div>
@@ -212,6 +229,13 @@ const DatosPersonales = () => {
                     </div>
                 </div>
             </form>
+
+            {/* Muestra la alerta si está visible */}
+            {alertState.visible && (
+                <div id="alertsElement" className={`alert ${alertState.type}`}>
+                    {alertState.message}
+                </div>
+            )}
         </>
     );
 };
--- acaestamos-front/src/componentes/PagesUsuario/Experiencia/Experiencia.jsx
+++ acaestamos-front/src/componentes/PagesUsuario/Experiencia/Experiencia.jsx
@@ -3,8 +3,11 @@ import './Experiencia.css';
 import client from '../../../Utils/axios.Client';
 import getUserId from '../../../Utils/getUserId';
 import { useNavigate, Link } from 'react-router-dom';
+import { useAlert } from '../../../Efectos/useAlert'
 
 const Experiencia = () => {
+
+    //Definicion de constantes
     const [perfil, setPerfil] = useState(null);
     const [Cargo, setCargo] = useState('');
     const [TipoEmpleo, setTipoEmpleo] = useState('');
@@ -14,57 +17,71 @@ const Experiencia = () => {
     const [CheckCargo, setCheckCargo] = useState('');
     const [DescripcionCargo, setDescripcionCargo] = useState('');
 
+    //Usa el hook useAlert para obtener el estado y la API de alerta
+    const [alertState, alertApi] = useAlert("alertsElement");
+
+    //Definicion de funciones
+    const showAlert = () => {
+
+        let hasError = true;
+
+        if (!Cargo || !TipoEmpleo || !NombredelaEmp || !FechaExpInicio || !FechaExpFin || !DescripcionCargo) {
+            alertApi.show("¡No llenaste correctamente los datos solicitados!", 'error');
+            hasError = false;
+        }
+    }
+
     useEffect(() => {
         const obtenerPerfil = async () => {
-          try {
-            const response = await client.get(`/Obtener-Perfil/${getUserId()}`)
-            setPerfil(response?.data);
-    
-            if (response?.data?.Cargo) setCargo(response?.data?.Cargo);
-            if (response?.data?.TipoEmpleo) setTipoEmpleo(response?.data?.TipoEmpleo);
-            if (response?.data?.NombredelaEmp) setNombredelaEmp(response?.data?.NombredelaEmp);
-            if (response?.data?.FechaExpInicio) setFechaExpInicio(response?.data?.FechaExpInicio);
-            if (response?.data?.FechaExpFin) setFechaExpFin(response?.data?.FechaExpFin);
-            if (response?.data?.CheckCargo) setCheckCargo(response?.data?.CheckCargo);
-            if (response?.data?.DescripcionCargo) setDescripcionCargo(response?.data?.DescripcionCargo);
-    
-          } catch (error) {
-            if (error.response) {
-              console.error('Error en la respuesta:', error.response.data);
-            } else if (error.request) {
-              console.error('Error en la solicitud:', error.request);
-            } else {
-              console.error('Error general:', error.message);
+            try {
+                const response = await client.get(`/Obtener-Perfil/${getUserId()}`)
+                setPerfil(response?.data);
+
+                if (response?.data?.Cargo) setCargo(response?.data?.Cargo);
+                if (response?.data?.TipoEmpleo) setTipoEmpleo(response?.data?.TipoEmpleo);
+                if (response?.data?.NombredelaEmp) setNombredelaEmp(response?.data?.NombredelaEmp);
+                if (response?.data?.FechaExpInicio) setFechaExpInicio(response?.data?.FechaExpInicio);
+                if (response?.data?.FechaExpFin) setFechaExpFin(response?.data?.FechaExpFin);
+                if (response?.data?.CheckCargo) setCheckCargo(response?.data?.CheckCargo);
+                if (response?.data?.DescripcionCargo) setDescripcionCargo(response?.data?.DescripcionCargo);
+
+            } catch (error) {
+                if (error.response) {
+                    console.error('Error en la respuesta:', error.response.data);
+                } else if (error.request) {
+                    console.error('Error en la solicitud:', error.request);
+                } else {
+                    console.error('Error general:', error.message);
+                }
             }
-          }
         };
-    
+
         obtenerPerfil();
-      }, []);
-    
-      const navigate = useNavigate();
-    
-      const GuardarPerfil = async (e) => {
+    }, []);
+
+    const navigate = useNavigate();
+
+    const GuardarPerfil = async (e) => {
         e.preventDefault();
         try {
-          const formData = new FormData();
-          formData.append('Cargo', Cargo);
-          formData.append('TipodeEmpleo', TipoEmpleo);
-          formData.append('FechaExpInicio', FechaExpInicio);
-          formData.append('NombredelaEmp', NombredelaEmp);
-          formData.append('FechaExpFin', FechaExpFin);
-          formData.append('CheckCargo', CheckCargo);
-          formData.append('DescripcionCargo', DescripcionCargo);
-    
-          const userId = getUserId();
-          const response = await client.post(`http://localhost:3000/Guardar-Perfil/${userId}`, formData);
-    
-          console.log(response.data);
-          navigate('/Actualizar-Cv')
+            const formData = new FormData();
+            formData.append('Cargo', Cargo);
+            formData.append('TipodeEmpleo', TipoEmpleo);
+            formData.append('FechaExpInicio', FechaExpInicio);
+            formData.append('NombredelaEmp', NombredelaEmp);
+            formData.append('FechaExpFin', FechaExpFin);
+            formData.append('CheckCargo', CheckCargo);
+            formData.append('DescripcionCargo', DescripcionCargo);
+
+            const userId = getUserId();
+            const response = await client.post(`http://localhost:3000/Guardar-Perfil/${userId}`, formData);
+
+            console.log(response.data);
+            navigate('/Actualizar-Cv')
         } catch (error) {
-          console.error('Error al guardar el perfil:', error);
+            console.error('Error al guardar el perfil:', error);
         }
-      };
+    };
 
     return (
         <>
@@ -87,47 +104,54 @@ const Experiencia = () => {
                                 <label htmlFor="tipoEmpleo" className="form-label">
                                     Tipo de empleo:
                                 </label>
-                                <input type="text" className="form-control" id="tipoEmpleo" placeholder="Part Time" defaultValue={perfil && perfil.TipoEmpleo} onChange={(e) => TipoEmpleo(e.target.value)}/>
+                                <input type="text" className="form-control" id="tipoEmpleo" placeholder="Part Time" defaultValue={perfil && perfil.TipoEmpleo} onChange={(e) => setTipoEmpleo(e.target.value)} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="nombreEmpresa" className="form-label">
                                     Nombre de la empresa:
                                 </label>
-                                <input type="text" className="form-control" id="nombreEmpresa" placeholder="Ej. MQQ" defaultValue={perfil && perfil.NombredelaEmp} onChange={(e) => setNombredelaEmp(e.target.value)}/>
+                                <input type="text" className="form-control" id="nombreEmpresa" placeholder="Ej. MQQ" defaultValue={perfil && perfil.NombredelaEmp} onChange={(e) => setNombredelaEmp(e.target.value)} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="fechaInicio" className="form-label">
                                     Fecha Inicio:
                                 </label>
-                                <input type="date" className="form-control" id="fechaInicio" placeholder="DD/MM/AAAA" defaultValue={perfil && perfil.FechaExpInicio} onChange={(e) => setFechaExpInicio(e.target.value)}/>
+                                <input type="date" className="form-control" id="fechaInicio" placeholder="DD/MM/AAAA" defaultValue={perfil && perfil.FechaExpInicio} onChange={(e) => setFechaExpInicio(e.target.value)} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="fechaFin" className="form-label">
                                     Fecha finalización:
                                 </label>
                                 <div className="form-check">
-                                    <input className="form-check-input me-2" type="checkbox" id="actualmenteCargo" defaultValue={perfil && perfil.CheckCargol} onChange={(e) => setCheckCargo(e.target.value)}/>
+                                    <input className="form-check-input me-2" type="checkbox" id="actualmenteCargo" defaultValue={perfil && perfil.CheckCargol} onChange={(e) => setCheckCargo(e.target.value)} />
                                     <label className="form-check-label" htmlFor="actualmenteCargo">
                                         Actualmente estoy en este cargo
                                     </label>
                                 </div>
-                                <input type="date" className="form-control" id="fechaFin" placeholder="DD/MM/AAAA" defaultValue={perfil && perfil.FechaExpFin} onChange={(e) => setFechaExpFin(e.target.value)}/>
+                                <input type="date" className="form-control" id="fechaFin" placeholder="DD/MM/AAAA" defaultValue={perfil && perfil.FechaExpFin} onChange={(e) => setFechaExpFin(e.target.value)} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="descripcionCargo" className="form-label">
                                     Descripción:
                                 </label>
-                                <input type="text" className="form-control" id="descripcionCargo" placeholder="Agrega una descripción del cargo" defaultValue={perfil && perfil.DescripcionCargo} onChange={(e) => setDescripcionCargo(e.target.value)}/>
+                                <input type="text" className="form-control" id="descripcionCargo" placeholder="Agrega una descripción del cargo" defaultValue={perfil && perfil.DescripcionCargo} onChange={(e) => setDescripcionCargo(e.target.value)} />
                             </div>
                             <div className="d-flex justify-content-center">
-                                <Link to="/Actualizar-Cv "type="button" className="btn btn-secondary btn-lg me-2">
+                                <Link to="/Actualizar-Cv " type="button" className="btn btn-secondary btn-lg me-2">
                                     Cancelar
                                 </Link>
-                                <button type="submit" className="btn btn-primary btn-lg">
+                                <button type="submit" className="btn btn-primary btn-lg" onClick={showAlert}>
                                     Guardar
                                 </button>
                             </div>
                         </form>
+
+                        {/* Muestra la alerta si está visible */}
+                        {alertState.visible && (
+                            <div id="alertsElement" className={`alert ${alertState.type}`}>
+                                {alertState.message}
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
