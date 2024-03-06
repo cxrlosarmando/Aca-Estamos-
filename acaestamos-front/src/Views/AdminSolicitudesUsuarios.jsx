fcb6bf28fb2513cf5eddc0ff836de72d07b0e0db
CarolinaReyes <karo.rg2312@gmail.com>
2024-03-06 03:04:23 -0300

    Implementación filtros en otras partes

--- acaestamos-front/src/Views/AdminSolicitudesUsuarios.jsx
+++ acaestamos-front/src/Views/AdminSolicitudesUsuarios.jsx
@@ -1,11 +1,9 @@
 import LayoutAdmin from "../Layout/LayoutAdmin"
 import SolicitudesUsuarios from "../componentes/PagesAdministrador/SolicitudesUsuarios/SolicitudesUsuarios";
-import FiltroAdmin from "../componentes/Filtros/FiltroAdmin/FiltroAdmin"
 
 const AdminSolicitudesUsuarios = () => {
     return (
         <LayoutAdmin>
-            <FiltroAdmin />
             <SolicitudesUsuarios />
         </LayoutAdmin>
     )
