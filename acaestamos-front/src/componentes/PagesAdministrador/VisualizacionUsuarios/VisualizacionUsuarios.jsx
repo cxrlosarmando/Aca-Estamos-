51890a5119e106595889f0eb88c916b241d49535
cxrlosarmando <carlos.armandor28@gmail.com>
2024-03-07 05:11:42 -0300

    Cambios minimos

--- acaestamos-front/src/componentes/PagesAdministrador/VisualizacionUsuarios/VisualizacionUsuarios.jsx
+++ acaestamos-front/src/componentes/PagesAdministrador/VisualizacionUsuarios/VisualizacionUsuarios.jsx
@@ -92,7 +92,7 @@ const VisualizacionUsuarios = () => {
                     </div>
                     <div className="conteiner fluid">
                         <div className="table-responsive">
-                            <table className="table table-bordered">
+                            <table className="table">
 
                                 <thead>
                                     <tr>
@@ -102,7 +102,7 @@ const VisualizacionUsuarios = () => {
                                         <th>Email</th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody className="table-group-divider">
                                     {
                                         filteredRecords.map((record, index) => (
                                             <tr key={index}>
