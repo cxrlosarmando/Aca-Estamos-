dbfb9937323e62631f3ba64b5ea37ac22014f728
Daniela Zuñiga <nta.danaz@gmail.com>
2024-03-07 18:11:20 -0300

    Filtro arreglado DZ

--- aca-estamos-back/src/controllers/admin-list-user.js
+++ aca-estamos-back/src/controllers/admin-list-user.js
@@ -2,7 +2,9 @@ const User = require("../models/user-model");
 
 const listaUsuariosAceptados = async (req, res) => {
     try {
-        const usuarios = await User.find({ aceptado: true }, 'Nombre Apellido Rut Email');
+        const usuarios = await User.find({ aceptado: true }, 
+            '  Nombre Apellido Telefono Rut FechaNacimiento Email Password EstadoCivil Rubro LinkedIn Disponibilidad ArchivoPDF1 ArchivoPDF ArchivoPDF3 ArchivoPDF4 ImagenPerfil NivelEducacional InstitucionEducativa Titulo FechaInicio FechaFinalizacion Descripcion Cargo TipoEmpleo NombredelaEmp FechaExpInicio FechaExpFin CheckCargo DescripcionCargo NombreCV TitularCV AcercadeCV ConocimientosCV CursosCV');
+
         console.log(usuarios);
 
         res.status(200).json({
